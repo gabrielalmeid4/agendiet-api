b7fb8f70a89c4723577813e5e7070811e2331bfd
Gabriel de Almeida <126924497+gabrielalmeid4@users.noreply.github.com>
2025-02-08 14:13:55 -0300

    alter nutricionista repo

--- domain/repositories/nutricionista_repo.py
+++ domain/repositories/nutricionista_repo.py
@@ -6,10 +6,10 @@ from domain.repositories.base_repo import BaseRepository
 class NutricionistaRepository(BaseRepository):
     async def salvar(self, nutricionista: Nutricionista):
         query = """
-        INSERT INTO nutricionista (latitude, longitude)
-        VALUES ($1, $2)
+        INSERT INTO nutricionista (nome, latitude, longitude)
+        VALUES ($1, $2, $3)
         """
-        await self.db.execute(query, nutricionista.latitude, nutricionista.longitude)
+        await self.db.execute(query, nutricionista.nome, nutricionista.latitude, nutricionista.longitude)
 
     async def get_all(self) -> List[Nutricionista]:
         query = "SELECT * FROM nutricionista"
@@ -28,7 +28,7 @@ class NutricionistaRepository(BaseRepository):
     async def update(self, id_nutricionista: int, nutricionista: Nutricionista):
         query = """
         UPDATE nutricionista
-        SET latitude = $2, longitude = $3
+        SET nome = $4, latitude = $2, longitude = $3
         WHERE id_nutricionista = $1
         """
-        await self.db.execute(query, id_nutricionista, nutricionista.latitude, nutricionista.longitude)
+        await self.db.execute(query, id_nutricionista, nutricionista.latitude, nutricionista.longitude, nutricionista.nome)

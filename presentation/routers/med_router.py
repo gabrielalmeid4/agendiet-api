7be54aed4fa77f059052e1adc58558e3e3431ca9
Gabriel de Almeida <126924497+gabrielalmeid4@users.noreply.github.com>
2025-02-09 13:19:29 -0300

    fix: paths url rotas

---
+++ presentation/routers/med_router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, Path
+from app.presentation.controllers.med_controller import MedicacaoController
+from app.domain.models.medicacao import Medicacao
+
+router = APIRouter(prefix="/medicacoes", tags=["Medicações"])
+
+@router.post("/registrar/{id_usuario}")
+async def registrar_medicacao(medicacao: Medicacao, id_usuario: int = Path(..., title="ID do Usuário"), medicacao_controller: MedicacaoController = Depends()):
+    return await medicacao_controller.registrar_medicacao(medicacao, id_usuario)
+
+@router.get("/get/{id_usuario}")
+async def get_medicacoes(id_usuario: int = Path(..., title="ID do Usuário"), medicacao_controller: MedicacaoController = Depends()):
+    return await medicacao_controller.get_medicacoes(id_usuario)
+
+@router.delete("/delete/{id_medicacao}")
+async def delete_medicacao(id_medicacao: int = Path(..., title="ID da Medicação"), medicacao_controller: MedicacaoController = Depends()):
+    return await medicacao_controller.delete_medicacao(id_medicacao)
+
+@router.put("/update/{id_medicacao}")
+async def update_medicacao(medicacao: Medicacao, id_medicacao: int = Path(..., title="ID da Medicação"), medicacao_controller: MedicacaoController = Depends()):
+    return await medicacao_controller.update_medicacao(medicacao, id_medicacao)
